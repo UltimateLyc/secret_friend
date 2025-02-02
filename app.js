36f7c2597bfd1b586419e945d43bef16bbc31678
Rafael N Ramos Garcia <ingrnrg@gmail.com>
2025-02-02 03:05:58 -0600

    Feat: Add new validation for empty input

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let arrayFriend = []
 
 function addFriend() {
-    console.log(document.getElementById('amigo').value)
-    arrayFriend.push(document.getElementById('amigo').value)
+    //console.log(document.getElementById('amigo').value.trim() === "" ? "esta vacio" : "no esta vacio")
+
+    document.getElementById('amigo').value.trim() === "" // trim( ) devuelve la cadena de texto despojada de los espacios en blanco en ambos extremos.
+        ? alert("Favor de ingresar un nombre valido") 
+        : arrayFriend.push(document.getElementById('amigo').value)
     console.log(arrayFriend.length)
     console.log(arrayFriend)
     clearInput()
