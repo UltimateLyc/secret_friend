b749323fc5301cc526ece75f66b65cf291794713
Rafael N Ramos Garcia <ingrnrg@gmail.com>
2025-02-02 19:51:16 -0600

    FEAT: add new function for generate a random number

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ let arrayFriend = []
 
 function addFriend() {
     //console.log(document.getElementById('amigo').value.trim() === "" ? "esta vacio" : "no esta vacio")
-
+    
     document.getElementById('amigo').value.trim() === "" // trim( ) devuelve la cadena de texto despojada de los espacios en blanco en ambos extremos.
-        ? alert("Favor de ingresar un nombre valido") 
-        : arrayFriend.push(document.getElementById('amigo').value)
-    //console.log(arrayFriend.length)
+    ? alert("Favor de ingresar un nombre valido") 
+    : arrayFriend.push(document.getElementById('amigo').value)
+    //console.log("🚀 ~ arrayFriend:", arrayFriend.length)
     //console.log(arrayFriend)
     createListFriends()
+    getSecretFriend()
     clearInput()
 }
 
@@ -18,5 +19,17 @@ function clearInput() {
 
 function createListFriends () {
     document.getElementById("listaAmigos").innerHTML = arrayFriend.map(friend => `<li>${friend}</li>`).join('')
+    /**
+     * map() transforma cada elemento en una cadena <li>Elemento</li>.
+     * join("") une los elementos sin comas.
+     * innerHTML inserta todo el HTML en el <ul>.
+     */
+}
 
+function getSecretFriend () {
+    let size = arrayFriend.length,
+    randomNumber = Math.floor(Math.random()*size)
+    console.log("🚀 ~ size:", size)
+    console.log("🚀 ~ randomNumber:", randomNumber)
+    
 }
